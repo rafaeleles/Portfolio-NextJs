6a7538f1913280e671a7d84f4deba87159c9b33e
Rafael Leles <68693480+rafaeleles@users.noreply.github.com>
2024-10-20 23:26:26 -0300

    ajustes

---
+++ src/WebSite/portfolio/src/app/components/navbar.tsx
@@ -0,0 +1,38 @@
+import { cn } from "@/lib/utils";
+import { useState } from "react";
+import { Menu, MenuItem, HoveredLink, ProductItem } from "./ui/navbar-menu";
+import Image from "next/image";
+
+export default function Navbar({ className }: { className?: string }) {
+    const [active, setActive] = useState<string | null>(null);
+    return (
+      <div
+        className={cn("fixed inset-x-0 max-w-2xl mx-auto z-50", className)}
+      >
+        <Menu setActive={setActive}>
+        <Image
+          src='/../../assets/eu2.jpg'
+          alt="rafaeleles"
+          height={208}
+          width={208}
+        />
+          <MenuItem setActive={setActive} active={active} item="Services">
+            <div className="flex flex-col space-y-4 text-sm">
+              <HoveredLink href="/web-dev">Web Development</HoveredLink>
+              <HoveredLink href="/interface-design">Interface Design</HoveredLink>
+              <HoveredLink href="/seo">Search Engine Optimization</HoveredLink>
+              <HoveredLink href="/branding">Branding</HoveredLink>
+            </div>
+          </MenuItem>
+          <MenuItem setActive={setActive} active={active} item="Pricing">
+            <div className="flex flex-col space-y-4 text-sm">
+              <HoveredLink href="/hobby">Hobby</HoveredLink>
+              <HoveredLink href="/individual">Individual</HoveredLink>
+              <HoveredLink href="/team">Team</HoveredLink>
+              <HoveredLink href="/enterprise">Enterprise</HoveredLink>
+            </div>
+          </MenuItem>
+        </Menu>
+      </div>
+    );
+  }
--- src/WebSite/portfolio/src/app/page.tsx
+++ src/WebSite/portfolio/src/app/page.tsx
@@ -1,29 +1,26 @@
+"use client";
 import Image from "next/image";
-import { BackgroundBeams } from "./components/background-beams";
-import { BackgroundGradient } from "./components/background-gradient";
-import euImg from '../assets/eu.png'
+import { BackgroundBeams } from "./components/ui/background-beams";
+import { BackgroundGradient } from "./components/ui/background-gradient";
+import euImg from "../assets/eu.png";
+import Navbar from "./components/navbar";
 
 export default function Home() {
   return (
-   <>
-   <div className="h-full w-full rounded-md bg-neutral-950 relative flex flex-col items-center justify-center antialiased">
-      <div className="max-w-3xl mx-auto mt-24 mb-10 p-4">
-        <h1 className="relative z-10 text-lg md:text-7xl bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-600  text-center font-sans font-bold"
-          style={{ backgroundImage: 'linear-gradient(to bottom, #4FA3FF, #2D3E9A)' }}
-          >
-          Ich bin Rafael Leles
-        </h1>
-        <Image
-          src={euImg}
-          alt="rafaeleles"
-          height={208}
-          width={208}
-        />
+    <>
+      <div className="h-full w-full rounded-md bg-neutral-950 relative flex flex-col items-center justify-center antialiased">
+        <Navbar className="top-2" />
+        <div className="max-w-3xl mx-auto mt-24 mb-10 p-4">
+          <h1 className="relative z-10 text-lg md:text-7xl 
+                          bg-clip-text text-transparent bg-gradient-to-b 
+                          from-neutral-200 to-neutral-600  
+                          text-center font-sans font-bold">
+            Ich bin Rafael Leles
+          </h1>
+        </div>
       </div>
-      
-    </div>
 
       <BackgroundBeams />
-   </>
+    </>
   );
 }
