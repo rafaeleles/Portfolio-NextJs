c5ba08e568fdb95aa370d984139ccd80d3df70d2
Rafael Leles <68693480+rafaeleles@users.noreply.github.com>
2024-10-23 22:29:58 -0300

    typewriter-effect.tsx

--- src/WebSite/portfolio/src/app/page.tsx
+++ src/WebSite/portfolio/src/app/page.tsx
@@ -6,17 +6,34 @@ import euImg from "../assets/eu.png";
 import Navbar from "./components/navbar";
 import { TextHoverEffect } from "./components/ui/text-hover-effect";
 import { TextGenerateEffect } from "./components/ui/text-generate-effect";
+import { TypewriterEffectSmooth } from "./components/ui/typewriter-effect";
 
-const words = `Oxygen gets you high. In a catastrophic emergency, we're taking giant, panicked breaths. Suddenly you become euphoric, docile. You accept your fate. It's all right here. Emergency water landing, six hundred miles an hour. Blank faces, calm as Hindu cows
-`;
+const words = [
+  {
+    text: "Build",
+  },
+  {
+    text: "awesome",
+  },
+  {
+    text: "apps",
+  },
+  {
+    text: "with",
+  },
+  {
+    text: "Aceternity.",
+    className: "text-blue-500 dark:text-blue-500",
+  },
+];
 
 export default function Home() {
   return (
     <>
       <div className="h-full w-full rounded-md relative flex flex-col items-center justify-center antialiased z-50 bg-transparent ">
         <Navbar className="top-5" />
-        <div className="mt-10">
-          <h1
+        <div style={{ marginTop: "9.5rem" }}>
+        <h1
             className="relative z-10 text-lg md:text-7xl 
                 bg-clip-text text-transparent bg-gradient-to-b 
                 from-neutral-200 to-neutral-600  
@@ -24,6 +41,8 @@ export default function Home() {
           >
             I'm Rafael Leles
           </h1>
+          <TypewriterEffectSmooth words={words} />
+
         </div>
       </div>
       <BackgroundBeams />
